8ff9513177d7e42990711e9d4ac5a59be10b8ddf
enesozmus <ensozmus@gmail.com>
2024-06-29 12:33:25 +0300

    Initial Commit

---
+++ DigitalMusicPlayerAppWithSwiftUI/DigitalMusicPlayerAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalMusicPlayerAppWithSwiftUIApp.swift
+//  DigitalMusicPlayerAppWithSwiftUI
+//
+//  Created by enesozmus on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DigitalMusicPlayerAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
